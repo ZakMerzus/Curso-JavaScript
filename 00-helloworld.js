6e2bd83c813486b7a36a345020e8852644a9ee87
Mau Bard <135636567+ZakMerzus@users.noreply.github.com>
2024-07-10 13:53:08 -0600

    modified:   00-helloworld.js 	new file:   01-variables.js

--- 00-helloworld.js
+++ 00-helloworld.js
@@ -1,1 +1,11 @@
+// COMENTARIOS CON DOBLE DIAGONAL
 
+console.log("Hola Mundo")
+console.log('Hola Mundo')
+
+// console.log(`Hola Mundo`)
+
+/* console.log("Hola Mundo")
+console.log("Hola Mundo")
+console.log("Hola Mundo")
+console.log("Hola Mundo") */
---
+++ 01-variables.js
@@ -0,0 +1,21 @@
+// var (ESTO CASI NO SE USA)
+var helloWorld = "Hola, JavaScript"
+console.log(helloWorld)
+
+helloWorld = "Hola de nuevo, JS"
+console.log (helloWorld)
+
+//ESTAS DOS SON LAS QUE MÁS SE OCUPAN Y LAS QUE SE DEBEN DE OCUPAR
+// let
+let helloWorld2 = "Hola JS 2"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo, JS 2"
+console.log (helloWorld2)
+
+// const (NO SE PUEDE MODIFICAR EL VALOR DE UNA CONSTANTE YA DECLARADA)
+const helloWorld3 = "Hola JS 3"
+console.log(helloWorld3)
+// Error porque no se puede modificar el valor de la constante ya declarada
+/* helloWorld3 = "Hola de nuevo, JS 2"
+console.log (helloWorld3) */
